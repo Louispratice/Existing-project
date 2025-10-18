9c02e8f96294b55f40499b954d3776f76d2a5a29
Louis <blessedlouistechnologylimited@gmail.com>
2025-10-18 08:23:58 +0100

    users api

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,35 @@ const users = [
     firstName:"john",
     lastName:"doe",
     age:25
-}
+},
+{
+    firstName:"anny",
+    lastName:"nike",
+    age:20
+},
+{
+    firstName:"Louis",
+    email:"louis@gmail.com",
+    lastName:"Emeka",
+},
 ]
 
 router.get('/',(req, res) =>{
-    console.log(users);
- res.send("hello from here");
+ res.send(users);
 });
+
+router.post('/', (req,res)=>{
+    const user = req.body;
+
+    console.log('user');
+
+
+    users.push(user);
+
+    res.send("user with the username $ {user.firstname} added to the database ");
+});
+
+
+
 
 export default router;
